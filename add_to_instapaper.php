7a839bef292a2a2a663df7deaf2d63cad9c1a18e
Darren Harkness <darren@staticred.com>
2025-12-20 17:28:41 -0800

    Now sorts articles by post time, so feeds don't bunch up by origin.

--- add_to_instapaper.php
+++ add_to_instapaper.php
@@ -16,8 +16,6 @@ date_default_timezone_set("America/Vancouver");
 // Reset counter. This will let us track the number of items processed. 
 $i = 0;
 
-// Instapaper Simple API URL:
-$insta_url = "https://www.instapaper.com/api/add";
 
 // Load RSS data (Feeds & last updated date)
 if (!$rss_arr = json_decode(file_get_contents($basedir . "/feeds.json"))) {
@@ -27,6 +25,8 @@ if (!$rss_arr = json_decode(file_get_contents($basedir . "/feeds.json"))) {
 // Grab the last updated time, so we can compare incoming feed items against 
 // what's already in the system
 $last_updated = !empty($rss_arr->last_updated) ? $rss_arr->last_updated : time() - 3600;
+
+$articles = array();
 
 // Loop through each of the feeds
 foreach ($rss_arr->feeds as $rss) {
@@ -53,41 +53,30 @@ foreach ($rss_arr->feeds as $rss) {
 
       // Compare the publication date to the last time we ran this script. 
       // and if it's older, skip to the next one
-      if (strtotime($item->pubDate) <= $last_updated) {
+      if ($item_time <= $last_updated) {
         continue;
       }
+      $articles[$item_time] = $item;
 
-      // var_dump($item->link[0]);
-
-      // OK, we have a new item. Let's post it to Instapaper. 
-      // Build a POST array
-      $post = [
-        "username" => $_ENV['INSTA_USER'],
-        "password" => $_ENV['INSTA_PW'],
-        "title" => (string) $item->title,
-        "url" => (string) $item->link,
-        "selection" => (string) substr(strip_tags($item->description),0,255)
-      ];
-      
-      
-      // Send the request and catch any errors
-      $ch = curl_init($insta_url);
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-      if (!$output = curl_exec($ch)) {
-        print "Curl error: " . curl_error($ch) . "\n";
-      }
-
-      // print ($output . "\n");
-      
-      
-      $i++;     
     }
   }
   // set the last_updated time once we've processed all feeds.
-//  $rss_arr->last_updated = time();    
+  $rss_arr->last_updated = time();    
 }
+
+print sizeof($articles) . " articles processed.\n";
+
+ksort($articles);
+
+foreach ($articles as $item) {
+  if (!post_to_instapaper($item)) {
+    error_log("Could not post {$item->title}");
+    return false;
+  }
+  $i++;
+}
+
+
 
 // Write back to feeds.json so we can store the last updated timestamp. 
 if (!file_put_contents("feeds.json", json_encode($rss_arr))) {
@@ -96,7 +85,33 @@ if (!file_put_contents("feeds.json", json_encode($rss_arr))) {
 // Print a status report. 
 print "Complete - processed {$i} items\n";
 
+function post_to_instapaper($item) {
+  
+  // Instapaper Simple API URL:
+  $insta_url = "https://www.instapaper.com/api/add";
+  
+  $post = [
+    "username" => $_ENV['INSTA_USER'],
+    "password" => $_ENV['INSTA_PW'],
+    "title" => (string) $item->title,
+    "url" => (string) $item->link,
+    "selection" => (string) substr(strip_tags($item->description),0,255)
+  ];
+  
+  // Send the request and catch any errors
+  $ch = curl_init($insta_url);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+  if (!$output = curl_exec($ch)) {
+    print "Curl error: " . curl_error($ch) . "\n";
+    return false;
+  }
 
+  return true;
+  
+  
+}
 
 
 
